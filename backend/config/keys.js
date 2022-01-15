fee8e2cb64224a124b51980ff33360dbc959f1d7
Abhijeet Singh Hada <69964453+AbhijeetSinghHada@users.noreply.github.com>
2022-01-15 19:41:01 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:Jake@12345@cluster0-8lbwj.mongodb.net/timer?retryWrites=true&w=majority",
+    mongoURI="mongodb+srv://db_admin:RdsHaJJm3gPx7nw@merncluster.j2klv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
